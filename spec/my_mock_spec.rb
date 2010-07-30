e050dd02d7a3e5650d4a9146cb66b4a548f48f43
Neill Pearman <neill@stackulous.com>
2010-07-30 21:21:05 +0100

    Finished all specs for the first full implementation.

--- spec/my_mock_spec.rb
+++ spec/my_mock_spec.rb
@@ -29,12 +29,16 @@ end
 
 # ooo err, test area
 class MyMock
-  def should_call method_name
-    raise NotCalled.new method_name unless @called == method_name
+  def initialize
+    @method_returns = {}
+    @method_calls = []
+  end
+  def called? method_name
+    raise NotCalled.new method_name unless @method_calls.include? method_name
   end
   def method_missing method_name, *args
     super method_name, *args unless args.empty?
-    @called = method_name
+    @method_calls << method_name
     return nil
   end
   def returns return_value
@@ -42,8 +46,13 @@ class MyMock
     self
   end
   def from method_name
-    instance_eval { define_method(method_name) { @return_value }}
-    #(class << self; self; end).instance_eval { define_method(method_name) { @return_value }}
+    @method_returns[method_name] = @return_value
+    self.class.instance_eval do
+      define_method(method_name) do
+        @method_calls << method_name
+        @method_returns[method_name]
+      end
+    end
   end
 end
 # aaaah, out of the test area
@@ -52,34 +61,6 @@ describe "mocking a parameter-less method call" do
   before(:each) do
     @my_mock = MyMock.new
   end
-
-  it "should make a mockery of blending toasters" do
-    @toaster = MyMock.new
-    @toaster.returns(true).from(:blends?)
-    Blender.new.blend(@toaster).should == "It blends!"
-  end
-
-  it "should make a mockery of toasting bread" do
-    @slice_of_bread = MyMock.new
-    Toaster.new.add(@slice_of_bread).press_switch
-    @slice_of_bread.called?(:toast)
-  end
-
-#  it "should track that a method has been called a number of times" do
-#    @my_mock.mock_method
-#    @my_mock.mock_method
-#    @my_mock.mock_method
-#    @my_mock.call_count(:mock_method).should == 3
-#  end
-#
-#  it "should allow you to check how many times a method has been called" do
-#    begin
-#      @my_mock.should_call(:mock_method).should == 0
-#    rescue Exception => e
-#      #not so good
-#      fail "You're not there yet: #{e.message}"
-#    end
-#  end
 
   it "should let you set expected return values on several methods" do
     expected_mock_method_result = "You're mockin' now!"
@@ -97,6 +78,16 @@ describe "mocking a parameter-less method call" do
     @my_mock.returns(expected_result).from(:mock_method)
     @my_mock.mock_method.should == "You're mockin' now!"
     @my_mock.mock_method.should == "You're mockin' now!"
+  end
+
+  it "should track method calls within individual mock instances" do
+    @my_mock.mock_method
+    another_mock = MyMock.new
+    begin
+      another_mock.called?(:mock_method)
+      fail "You're not there yet; mock_method was only called on one instance of MyMock."
+    rescue NotCalled
+    end
   end
 
   it "should only define the result on the specific mock instance" do
@@ -149,10 +140,25 @@ describe "mocking a parameter-less method call" do
     @my_mock.mock_method.should be_nil
   end
 
+  it "should not complain if two different methods have been called" do
+    @my_mock.mock_method
+    @my_mock.another_method
+    begin
+      @my_mock.called?(:mock_method)
+    rescue NotCalled => e
+      fail "Should not have raised a NotCalled error...  mock_method was called on the mock!\n[#{e.message}]"
+    end
+    begin
+      @my_mock.called?(:another_method)
+    rescue NotCalled => e
+      fail "Should not have raised a NotCalled error...  another_method was called on the mock!\n[#{e.message}]"
+    end
+  end
+
   it "should not complain if a method has been called" do
     @my_mock.mock_method
     begin
-      @my_mock.should_call(:mock_method)
+      @my_mock.called?(:mock_method)
     rescue NotCalled => e
       fail "Should not have raised a NotCalled error...  mock_method was mocked!\n[#{e.message}]"
     end
@@ -178,8 +184,8 @@ describe "mocking a parameter-less method call" do
 
   it "should tell you that a method wasn't called, if you ask" do
     begin
-      @my_mock.should_call(:not_called)
-      fail "Method was not called, but NotCalled(method_name) exception was not raised...."
+      @my_mock.called?(:not_called)
+      fail "You're not there yet; Method was not called, but NotCalled(method_name) exception was not raised...."
     rescue NotCalled
       # all good
     rescue Exception => e
@@ -190,5 +196,37 @@ describe "mocking a parameter-less method call" do
 
   it "should be defined as a class!!" do
     defined?(MyMock).should be_true, "MyMock hasn't been defined as a class!"
+  end
+end
+
+describe "testing that your new mocking class works" do
+  it "should make a mockery of toasting bread" do
+    @slice_of_bread = MyMock.new
+    Toaster.new.add(@slice_of_bread).press_switch
+    @slice_of_bread.called?(:toast)
+  end
+
+  it "should make a mockery of blending toasters" do
+    @toaster = MyMock.new
+    @toaster.returns(true).from(:blends?)
+    Blender.new.blend(@toaster).should == "It blends!"
+  end
+end
+
+describe "mocking a parameterless method a number of times" do
+  it "should track that a method has been called a number of times" do
+    @my_mock.mock_method
+    @my_mock.mock_method
+    @my_mock.mock_method
+    @my_mock.call_count(:mock_method).should == 3
+  end
+
+  it "should allow you to check how many times a method has been called" do
+    begin
+      @my_mock.called?(:mock_method).should == 0
+    rescue Exception => e
+      #not so good
+      fail "You're not there yet: #{e.message}"
+    end
   end
 end
