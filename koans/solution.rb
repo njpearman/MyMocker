41ada53d809aa68fdd92dba76e3ee396d776973c
Neill Pearman <neill@stackulous.com>
2010-08-12 18:50:57 +0100

    Removed my_mock.rb and commented solution.

--- koans/solution.rb
+++ koans/solution.rb
@@ -1,11 +1,11 @@
 #class MyMock
 #  def initialize
-#    @method_returns, @method_calls = {}, []
+#    @method_calls = []
 #  end
 #
 #  def called? method_name
 #    raise NotCalled.new unless @method_calls.include? method_name
-#    @method_calls.count {|method| method_name == method }
+#    @method_calls.count {|method| method == method_name }
 #  end
 #
 #  def method_missing method_name, *args
@@ -14,18 +14,22 @@
 #    return nil
 #  end
 #
-#  def returns return_value
-#    @return_value = return_value
-#    self
+#  def returns value
+#    @value = value
+#    return self
 #  end
 #
 #  def from method_name
-#    @method_returns[method_name] = @return_value
-#    @return_value = nil
-#    self.class.instance_eval do
+#    define method_name, @value
+#    @value = nil
+#  end
+#
+#  private
+#  def define method_name, return_value
+#    (class<<self;self;end).instance_eval do
 #      define_method(method_name) do
 #        @method_calls << method_name
-#        @method_returns[method_name]
+#        return return_value
 #      end
 #    end
 #  end
