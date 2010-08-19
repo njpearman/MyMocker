33e8851b395bf516fc93b9d77e89aefb22bcede4
Neill Pearman <neill@stackulous.com>
2010-08-20 00:34:52 +0100

    Added some comments and removed a zombie git conflict file.  Spooky.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,18 +2,21 @@ sample_solution_path = File.join(File.expand_path(File.dirname(__FILE__)), '..',
 require sample_solution_path if File.exists? sample_solution_path
 require File.join(File.expand_path(File.dirname(__FILE__)), 'koan_world.rb')
 
+# This is a hammer. A hammer hits things
 class Hammer
   def hit thing
     thing.hit
   end
 end
 
+# This is a blender.  A blender blends things.
 class Blender
   def blend thing
     "It blends!" if thing.blends?
   end
 end
 
+# This is a toaster.  A toaster toasts things (but never with champagne)
 class Toaster
   def add food
     @food = food
@@ -25,10 +28,9 @@ class Toaster
   end
 end
 
-class ToasterMalfunction < Exception; end
-
 # This is an exception used by MyMock.  Moved it out of the test bay if you dare
 # use this stuff.
 class NotCalled < Exception; end
 
+# populate the cucumber world with the Koan landscape
 KoanWorld.populate(self)
