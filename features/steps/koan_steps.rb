57c36578947751fa77db4a1ffb4a2d00e94e4716
Neill Pearman <neill@stackulous.com>
2010-10-06 00:52:06 +0100

    Removed unused cucumber step.

--- features/steps/koan_steps.rb
+++ features/steps/koan_steps.rb
@@ -1,7 +1,3 @@
-Given /^the koan is complete$/ do
-  # Her name is Yoshimi, she's a black belt in karate
-end
-
 When /^a new MyMock instance is created$/ do
   defined?(MyMock).should be_true, koan_fail_message("MyMock hasn't been defined as a class.  Put it in the my_mock.rb file.")
   @my_mock = MyMock.new
