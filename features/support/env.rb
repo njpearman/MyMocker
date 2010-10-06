65e927ab75b644c88265067f4354b554a3ace3a2
Neill Pearman <neill@stackulous.com>
2010-10-06 01:12:37 +0100

    Tidied up the cucumber env.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,13 @@
-sample_solution_path = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'lib', 'my_mock.rb')
-examples_path = File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'lib', 'examples')
-Dir.foreach(examples_path) {|file| require File.join(examples_path, file) if file =~ /\.rb$/ }
+current_path = File.expand_path(File.dirname(__FILE__))
+sample_solution_path = File.join(current_path, '..', '..', 'lib', 'my_mock.rb')
+examples_path = File.join(current_path, '..', '..', 'lib', 'examples')
+Dir.foreach(examples_path) do |file|
+  require File.join(examples_path, file) if file =~ /\.rb$/
+end
 require sample_solution_path if File.exists? sample_solution_path
-require File.join(File.expand_path(File.dirname(__FILE__)), 'koan_world.rb')
+require File.join(current_path, 'koan_world.rb')
 
-# This is an exception used by MyMock.  Moved it out of the test bay if you dare
+# This is an exception used by MyMock.  Move it out of the test bay if you dare
 # use this stuff.
 class NotCalled < Exception; end
 
